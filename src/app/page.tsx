4b73b9faa2eede1770af41dd0d518a22d05ec5d5
AdriALV2005 <adrimon7221@gmail.com>
2024-10-13 22:26:50 -0500

    Middleware para la sesion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
   return (
     <div className="">
-      <Button variant="muted">Click me</Button>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
