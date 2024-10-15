33d4290b53e78cd637a8a3be182ac3a6253e1c17
AdriALV2005 <adrimon7221@gmail.com>
2024-10-15 14:29:35 -0500

    Componente creacion workspace

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,10 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { InferRequestType, InferResponseType } from "hono";
+import { InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
-type RequestType = InferRequestType<(typeof client.api.auth.logout)["$post"]>;
 
 export const useLogout = () => {
   const router = useRouter();
@@ -12,11 +12,18 @@ export const useLogout = () => {
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth.logout["$post"]();
+      if (!response.ok) {
+        throw new Error("Failed to logout");
+      }
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Logged out");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+    onError: () => {
+      toast.error("Failed to log out");
     },
   });
   return mutation;
