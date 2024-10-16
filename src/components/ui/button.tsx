a59cff0f85cb95b49eb6eee64d86fa9bbad988f0
AdriALV2005 <adrimon7221@gmail.com>
2024-10-16 11:12:13 -0500

    Cambiando y optimizando queries

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,19 +12,20 @@ const buttonVariants = cva(
         primary:
           "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
         destructive:
-          "bg-[#fe3332] text-neutral-100 hover:bg-[#ff4d4d] hover:text-white",
+          "bg-[#fe3332] border-none text-neutral-100 hover:bg-[#ff4d4d] hover:text-white",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary: "bg-white text-black hover:bg-neutral-100",
         ghost:
           "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground",
         muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80",
-        teritary:"bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none",
+        teritary:
+          "bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-8 rounded-md px-3",
-        xs:"h-7 rounded-md px-2 text-xs", 
+        xs: "h-7 rounded-md px-2 text-xs",
         lg: "h-12 rounded-md px-8",
         icon: "h-8 w-8",
       },
